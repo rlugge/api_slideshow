46763f1877f4ac8365b3782105cb878be18e99ba
Ronald J. Lugge III <ronlugge@gmail.com>
2015-03-12 17:40:38 -0700

    Explicitly catch different authentication exceptions, for better handling

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ApiController < ApplicationController
   class Unauthenticated < StandardError; end
+  class InvalidAuthorizationToken < Unauthenticated; end
+  class MissingAuthorizationToken < Unauthenticated; end
   protect_from_forgery with: :null_session
   before_filter :authenticate!
 
@@ -14,13 +16,21 @@ class Api::V1::ApiController < ApplicationController
     render json: { error: "System cannot authenticate you without a valid token based authentication header."}
   end
 
+  rescue_from MissingAuthorizationToken do |exception|
+    render json: { error: "Missing authentication token header"}
+  end
+
+  rescue_from InvalidAuthorizationToken do |exception|
+    render json: { error: "Provided authentication token did not match any valid sessions."}
+  end
+
   def authenticate!
     authenticate_with_http_token do |token, options|
       session=Session.where(:token=>token).first
-      raise Unauthenticated.new unless session
+      raise InvalidAuthorizationToken.new unless session
       @current_user=session.user
     end
-    raise Unauthenticated.new unless @current_user
+    raise MissingAuthorizationToken.new unless @current_user
   end
 
   def current_user
