b2af67158e9e212aa399a9608f72d467eedf7aee
Ronald J. Lugge III <ronlugge@gmail.com>
2015-03-12 17:35:13 -0700

    Handle invalid token scenario

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,13 +3,22 @@ class Api::V1::ApiController < ApplicationController
   protect_from_forgery with: :null_session
   before_filter :authenticate!
 
+  rescue_from Exception do |exception|
+    error={
+      :exception=>exception.message
+    }
+    render json:error, :status=>500
+  end
+
   rescue_from Unauthenticated do |exception|
     render json: { error: "System cannot authenticate you without a valid token based authentication header."}
   end
 
   def authenticate!
     authenticate_with_http_token do |token, options|
-      @current_user=Session.where(:token=>token).first.user
+      session=Session.where(:token=>token).first
+      raise Unauthenticated.new unless session
+      @current_user=session.user
     end
     raise Unauthenticated.new unless @current_user
   end
