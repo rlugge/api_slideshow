1bbfbb4a3750f74c6ac8cfd52befa75405efced9
Ronald J. Lugge III <ronlugge@gmail.com>
2015-03-12 17:31:10 -0700

    Now catching authentication error

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::ApiController < ApplicationController
   protect_from_forgery with: :null_session
   before_filter :authenticate!
 
+  rescue_from Unauthenticated do |exception|
+    render json: { error: "System cannot authenticate you without a valid token based authentication header."}
+  end
+
   def authenticate!
     authenticate_with_http_token do |token, options|
       @current_user=Session.where(:token=>token).first.user
