3a650b64cb77ba76ddf6b3adbcc5ecf6d8c2ee35
Ronald J. Lugge III <ronlugge@gmail.com>
2015-03-12 17:25:55 -0700

    Catch missing auth token with specific exception

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ApiController < ApplicationController
+  class Unauthenticated < StandardError; end
   protect_from_forgery with: :null_session
   before_filter :authenticate!
 
   def authenticate!
-    authenticate_or_request_with_http_token do |token, options|
+    authenticate_with_http_token do |token, options|
       @current_user=Session.where(:token=>token).first.user
     end
+    raise Unauthenticated.new unless @current_user
   end
 
   def current_user
